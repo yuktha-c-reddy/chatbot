8df4292304fecfe3c851464b1a5026ea08b65d35
yuktha-c <yuktha.c04@gmail.com>
2024-08-06 20:40:50 +0530

    Minute changes

--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -2,22 +2,17 @@ import React from 'react';
 import './Screen.css'
 import { Link } from 'react-router-dom';
 const Screen = () => {
-  const backgroundImageUrl = 'https://images.unsplash.com/photo-1663657473556-a8928dc06be9?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
-
-  const screenStyle = {
-    width: '100%',
-    height: '100vh',
-    backgroundImage: `url(${backgroundImageUrl})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-  };
-
   return (
     <div className='home'>
-       <h1>Talk to your Data!</h1>
+       <h1>MyHR!</h1>
        <h3>Role based interactions</h3>
        <Link className='start-btn' to='/login'>Start</Link>
+       <div className="video-container">
+          <video autoPlay muted>
+            <source src="./IBM_watsonx_ai_animation.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
     </div>
   )
 }
